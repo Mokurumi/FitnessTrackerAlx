550ecdaf23a74d54fe5c094740b634f606a41e65
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-02-26 00:03:24 +0300

    use myscql

--- website/__init__.py
+++ website/__init__.py
@@ -10,7 +10,7 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://holberton_user:projectcorrection280hbtn@localhost/fitnesstracker'
     db.init_app(app)
 
     from .views import views
@@ -20,7 +20,7 @@ def create_app():
     app.register_blueprint(auth, url_prefix='/')
 
     from .models import User, Note
-    
+
     with app.app_context():
         db.create_all()
 
@@ -36,6 +36,6 @@ def create_app():
 
 
 def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
+    with app.app_context():
+        db.create_all()
         print('Created Database!')
